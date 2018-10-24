aea5194608870f30d9dde62ab333e73f6a594031
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-10-24 18:51:11 +0200

    add EMUI description

---
+++ app/src/main/java/it/geosolutions/savemybike/ui/utils/PowerManager.java
@@ -0,0 +1,95 @@
+package it.geosolutions.savemybike.ui.utils;
+
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.CompoundButton;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import it.geosolutions.savemybike.R;
+
+public class PowerManager {
+    public static List<Intent> POWERMANAGER_INTENTS = Arrays.asList(
+            new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+            new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.power.ui.HwPowerManagerActivity")),
+            new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
+            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
+            new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
+            new Intent().setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
+            new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.entry.FunctionActivity")).setData(android.net.Uri.parse("mobilemanager://function/entry/AutoStart"))
+    );
+
+
+    public static void startPowerSaverIntent(Context context) {
+        SharedPreferences settings = context.getSharedPreferences("ProtectedApps", Context.MODE_PRIVATE);
+        boolean skipMessage = settings.getBoolean("skipProtectedAppCheck", false);
+        if (!skipMessage) {
+            final SharedPreferences.Editor editor = settings.edit();
+            boolean foundCorrectIntent = false;
+            for (Intent intent : POWERMANAGER_INTENTS) {
+                if (isCallable(context, intent)) {
+                    foundCorrectIntent = true;
+                    final AppCompatCheckBox dontShowAgain = new AppCompatCheckBox(context);
+                    dontShowAgain.setText("Do not show again");
+                    dontShowAgain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            editor.putBoolean("skipProtectedAppCheck", isChecked);
+                            editor.apply();
+                        }
+                    });
+                    Spanned message = Html.fromHtml(String.format("%s requires to be enabled in 'Protected Apps' to function properly.%n", context.getString(R.string.app_name)));
+                    switch (intent.getComponent().getClassName()) {
+                        case "com.huawei.systemmanager.power.ui.HwPowerManagerActivity":
+                            message = Html.fromHtml(context.getString(R.string.huauei_emui_powersaving_instructions));
+                            break;
+                    }
+                    new AlertDialog.Builder(context)
+                            .setTitle(Build.MANUFACTURER +context.getResources().getString(R.string.protected_apps))
+                            .setMessage(message)
+                            .setView(dontShowAgain)
+                            .setPositiveButton(R.string.go_to_settings, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                        // TODO give instructions for specific manager
+                                        context.startActivity(intent);
+
+
+                                }
+                            })
+                            .setNegativeButton(android.R.string.cancel, null)
+                            .show();
+                    break;
+                }
+            }
+            if (!foundCorrectIntent) {
+                editor.putBoolean("skipProtectedAppCheck", true);
+                editor.apply();
+            }
+        }
+    }
+
+
+    private static boolean isCallable(Context context, Intent intent) {
+        List<ResolveInfo> list = context.getPackageManager().queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+}
