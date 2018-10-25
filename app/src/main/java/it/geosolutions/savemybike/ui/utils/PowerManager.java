19ab4d1a841926d79596c79b5467495c6f1f380c
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-10-25 14:26:04 +0200

    Improved UI for initial dialog

--- app/src/main/java/it/geosolutions/savemybike/ui/utils/PowerManager.java
+++ app/src/main/java/it/geosolutions/savemybike/ui/utils/PowerManager.java
@@ -9,10 +9,15 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -47,25 +52,22 @@ public class PowerManager {
             for (Intent intent : POWERMANAGER_INTENTS) {
                 if (isCallable(context, intent)) {
                     foundCorrectIntent = true;
-                    final AppCompatCheckBox dontShowAgain = new AppCompatCheckBox(context);
-                    dontShowAgain.setText("Do not show again");
-                    dontShowAgain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                            editor.putBoolean("skipProtectedAppCheck", isChecked);
-                            editor.apply();
-                        }
-                    });
-                    Spanned message = Html.fromHtml(String.format("%s requires to be enabled in 'Protected Apps' to function properly.%n", context.getString(R.string.app_name)));
+
+
+                    String message = context.getResources().getString(R.string.no_powersaving_instructions);
                     switch (intent.getComponent().getClassName()) {
                         case "com.huawei.systemmanager.power.ui.HwPowerManagerActivity":
-                            message = Html.fromHtml(context.getString(R.string.huauei_emui_powersaving_instructions));
+                            message = context.getString(R.string.huauei_emui_powersaving_instructions);
                             break;
                     }
+                    CompoundButton.OnCheckedChangeListener dontShowListener = (CompoundButton buttonView, boolean isChecked) -> {
+                        editor.putBoolean("skipProtectedAppCheck", isChecked);
+                        editor.apply();
+                    };
+                    View content = getContentView(context, message, dontShowListener);
                     new AlertDialog.Builder(context)
-                            .setTitle(Build.MANUFACTURER +context.getResources().getString(R.string.protected_apps))
-                            .setMessage(message)
-                            .setView(dontShowAgain)
+                            .setTitle(Build.MANUFACTURER + " " + context.getResources().getString(R.string.protected_apps))
+                            .setView(content)
                             .setPositiveButton(R.string.go_to_settings, new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                         // TODO give instructions for specific manager
@@ -84,6 +86,24 @@ public class PowerManager {
                 editor.apply();
             }
         }
+    }
+
+    @NonNull
+    private static View getContentView(Context context, String message, CompoundButton.OnCheckedChangeListener dontShowListener) {
+        View content = LayoutInflater.from(context).inflate(R.layout.powermanagement_dialog,null);
+        AppCompatCheckBox dontShowAgain = content.findViewById(R.id.pm_dialog_dont_show_again);
+
+        dontShowAgain.setOnCheckedChangeListener(dontShowListener);
+        TextView description = content.findViewById(R.id.pm_dialog_description);
+        description.setText(
+                Html.fromHtml(context.getResources().getString(R.string.powersaving_description) )
+        );
+        TextView instructions = content.findViewById(R.id.pm_dialog_instructions);
+        instructions.setText(Html.fromHtml(message));
+
+        // WebView instructions = content.findViewById(R.id.pm_dialog_instructions);
+        // instructions.loadData(message, "text/html", "utf-8");
+        return content;
     }
 
 
